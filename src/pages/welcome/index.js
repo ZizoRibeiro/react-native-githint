cef1e3d07ee4c381c9083762fad110602689a32f
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-11-17 15:56:55 -0800

    added navigation route to user page

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { NavigationActions } from 'react-navigation';
+
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
+
 export default class Welcome extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  };
+
   static navigationOptions = {
     // delete the page header
-    header: null, 
+    header: null,
   };
+
+  navigateToUser = () => {
+    const { dispatch } = this.props.navigation;
+
+    const resetAction = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'User' }),
+      ],
+    });
+
+    dispatch(resetAction);
+  };
+
+
   render() {
     return (
       <View style={styles.container}>
@@ -15,12 +40,12 @@ export default class Welcome extends Component {
           You need to Login In to proceed.
         </Text>
 
-        <TextInput 
+        <TextInput
           style={styles.input}
           placeholder="insert you User name"
         />
 
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <TouchableOpacity style={styles.button} onPress={this.navigateToUser}>
           <Text style={styles.buttonText}>Proceed</Text>
         </TouchableOpacity>
       </View>
